87c3fea181d9a9dedcd93c9146a7465c78b394c0
todd <38468856@qq.com>
2019-08-02 17:07:24 +0800

    update

--- src/main/java/com/ghz/testweb/App.java
+++ src/main/java/com/ghz/testweb/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! i am guo2" );
+        System.out.println( "Hello World! i am todd" );
     }
 }
